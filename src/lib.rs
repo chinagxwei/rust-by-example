fad73f906a22458f248dad6c2ff1fbeb1fdb405e
kumatata <chinagxwei@gmail.com>
2020-01-14 15:54:01 +0800

    add example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod hello_example;
 pub mod primitives_example;
 pub mod custom_types_example;
+pub mod variable_bindings_example;
---
+++ src/variable_bindings_example/declare_example.rs
@@ -0,0 +1,29 @@
+fn declare_example() {
+    // Declare a variable binding
+    let a_binding;
+
+    {
+        let x = 2;
+        a_binding = x * x;
+    }
+
+    println!("a binding: {}", a_binding);
+
+    let another_binding;
+
+//    println!("another binding: {}", another_binding);
+
+    another_binding = 1;
+
+    println!("another binding: {}", another_binding);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_declare_example() {
+        declare_example();
+    }
+}
