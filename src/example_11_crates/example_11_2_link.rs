fa2825cd8907e3766313f1c58580bdede788e33b
kumatata <chinagxwei@gmail.com>
2020-01-20 14:52:21 +0800

    add example

--- src/example_11_crates/example_11_2_link.rs
+++ src/example_11_crates/example_11_2_link.rs
@@ -2,10 +2,11 @@
 // rustc ./src/example_11_crates/example_11_2_link.rs --extern example_11_1_lib=libexample_11_1_lib.rlib && example_11_2_link
 // 执行以上命令可链接一个库
 
-extern crate example_11_1_lib;
-
-fn main() {
-    example_11_1_lib::public_function();
-
-    example_11_1_lib::indirect_access();
-}
+// 执行时关闭以下代码注释
+//extern crate example_11_1_lib;
+//
+//fn main() {
+//    example_11_1_lib::public_function();
+//
+//    example_11_1_lib::indirect_access();
+//}
