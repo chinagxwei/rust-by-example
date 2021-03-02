1fa39adc3e29582b057d11c18cc48d04777452c6
kumatata <chinagxwei@gmail.com>
2021-03-02 16:09:53 +0800

    add example

--- src/example_17_macros/mod.rs
+++ src/example_17_macros/mod.rs
@@ -3,15 +3,16 @@ mod example_17_2_dry;
 mod example_17_3_dsl;
 mod example_17_4_variadics;
 
-/// block
-/// expr 用于表达式
-/// ident 用于变量名或函数名
-/// item
-/// pat (模式 pattern)
-/// path
-/// stmt (语句 statement)
-/// tt (标记树 token tree)
-/// ty (类型 type)
+/// item: 条目，比如函数、结构体、模组等。
+/// block: 区块(即由花括号包起的一些语句加上/或是一项表达式)。
+/// stmt: 语句
+/// pat: 模式
+/// expr: 表达式
+/// ty: 类型
+/// ident: 标识符
+/// path: 路径 (例如 foo, ::std::mem::replace, transmute::<_, int>, …)
+/// meta: 元条目，即被包含在 #[…]及#![…]属性内的东西。
+/// tt: 标记树
 
 macro_rules! say_hello {
     () => {
